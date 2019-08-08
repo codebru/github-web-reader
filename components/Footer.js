3b2e36b5259801704c922de220a37454634528c0
Philip Bruland <philipbruland@gmail.com>
2019-08-08 23:52:19 +0100

    Add correct links to to the footer
    - link to the original source repo
    - link to the project creator codebru

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,19 @@
 import Link from 'next/link';
+import { githubUser, githubRepo } from '../config';
 
 function Footer() {
   return (
     <div>
-      <p>This is a website by Philip Bruland</p>
+      <p>
+        This is a website by
+        <Link href="https://github.com/codebru">
+          <a> Codebru</a>
+        </Link>
+      </p>
       <br />
       <p>
-        <Link href="https://github.com/codebru/full-web-dev">
         The original source of the information can be found
+        <Link href={`https://github.com/${githubUser}/${githubRepo}`}>
           <a> here</a>
         </Link>
         .
