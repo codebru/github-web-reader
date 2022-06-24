65eb36765e641f51b6223f7c4a6a6eece65395a1
Philip Bruland <philip.bruland@ovoenergy.com>
2022-06-24 15:40:05 +0100

    Add test to cover the navigation to a subpage flat in the structure

--- cypress/e2e/readInformation.cy.js
+++ cypress/e2e/readInformation.cy.js
@@ -3,4 +3,10 @@ describe('GIVEN I am a user that wants to read the information on the site', () 
     cy.visit('http://0.0.0.0:3000');
     cy.contains('Sample markdown project for testing the github-web-reader codebase');
   });
+
+  it('WHEN I decide to read a sub page AND select that page from the navigation THEN it is displayed to me', () => {
+    cy.visit('http://0.0.0.0:3000');
+    cy.contains('subpage').click();
+    cy.contains('Subpage Title');
+  });
 });
